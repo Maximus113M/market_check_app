e14df39bca1520be18d7854f6f8f1c0cbef38bc7
Marly Rangel <rangelmarly@gmail.com>
2024-02-15 11:23:57 -0500

    store color and image

--- market_check/lib/features/stores/presentation/screens/store/store_screen.dart
+++ market_check/lib/features/stores/presentation/screens/store/store_screen.dart
@@ -16,7 +16,13 @@ class StoreScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.network('${RemoteUrls.currentImagesUrl}${store.poster!}',
+          (store.poster == null)
+          ?Image.asset('assets/Images/stores/establecimiento-base.jpg',
+          width: double.infinity,
+          height: ScreenSize.absoluteHeight * 0.33,
+          fit: BoxFit.cover,
+          )
+          :Image.network('${RemoteUrls.currentImagesUrl}${store.poster!}',
           width: double.infinity,
           height: ScreenSize.absoluteHeight * 0.4,
           fit: BoxFit.cover),
--- market_check/lib/features/stores/presentation/widgets/store/store_container_screen.dart
+++ market_check/lib/features/stores/presentation/widgets/store/store_container_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:market_check/config/utils/constans/app_colors.dart';
 import 'package:market_check/config/utils/screen_size.dart';
 
 class StoreContainerScreen extends StatelessWidget {
@@ -10,9 +9,9 @@ class StoreContainerScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(top: ScreenSize.absoluteHeight * 0.3),
       padding: EdgeInsets.symmetric(vertical: ScreenSize.absoluteHeight * 0.03, horizontal: ScreenSize.width * 0.05),
-      decoration: const BoxDecoration(
-        color: AppColors.appGray,
-        borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30))
+      decoration: BoxDecoration(
+        color: Colors.grey.shade300,
+        borderRadius: const BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30))
       ),
       child: const Column(
         children: [
