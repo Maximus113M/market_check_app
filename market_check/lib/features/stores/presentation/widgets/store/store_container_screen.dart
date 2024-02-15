590ab9e8b32c749008ea1de4116a17ddf271f036
Marly Rangel <rangelmarly@gmail.com>
2024-02-15 14:34:08 -0500

    delete description store container

--- market_check/lib/features/stores/presentation/widgets/store/store_container_screen.dart
+++ market_check/lib/features/stores/presentation/widgets/store/store_container_screen.dart
@@ -13,17 +13,22 @@ class StoreContainerScreen extends StatelessWidget {
     final StoreModel store = context.read<StoresProvider>().currentStore!;
     return Container(
       margin: EdgeInsets.only(top: ScreenSize.absoluteHeight * 0.3),
-      padding: EdgeInsets.symmetric(vertical: ScreenSize.absoluteHeight * 0.03, horizontal: ScreenSize.width * 0.05),
+      padding: EdgeInsets.symmetric(
+          vertical: ScreenSize.absoluteHeight * 0.03,
+          horizontal: ScreenSize.width * 0.05),
       decoration: BoxDecoration(
         color: Colors.grey.shade300,
-        borderRadius: const BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30))
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
       ),
       child: Column(
         children: [
-          Text(store.name, style: const TextStyle(fontSize: 25)),
-          const SizedBox(height: 25),
-          const Text(
-              'Nostrud pariatur incididunt aute est incididunt magna commodo dolor quis consequat quis et pariatur labore.Commodo labore ad est ut magna ad aliquip culpa laboris Lorem enim veniam non laboris.Non aliqua culpa proident tempor ad ea.'),
+          Text(
+            store.name,
+            style: const TextStyle(fontSize: 25),
+          ),
           const SizedBox(height: 35),
           const StoreMenuScreen()
         ],
