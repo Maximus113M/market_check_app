263c49f4ba453969543990fdd2646f8e156d14a7
Maximus113M <camilomantillaq13@gmail.com>
2024-02-16 01:15:20 -0500

    REFACTOR(profileScreen): Update screen design

--- market_check/lib/features/profile/presentation/screens/profile_screen.dart
+++ market_check/lib/features/profile/presentation/screens/profile_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:market_check/config/shared/widgets/appbars/custom_appbar.dart';
-import 'package:market_check/config/utils/constans/app_assets.dart';
+import 'package:market_check/config/services/auth/auth_service.dart';
+
 import 'package:market_check/config/utils/utils.dart';
+import 'package:market_check/config/shared/widgets/appbars/custom_appbar.dart';
 
 class ProfileScreen extends StatelessWidget {
   static const String name = "perfile-screen";
@@ -14,17 +15,90 @@ class ProfileScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
           Container(
             margin: EdgeInsets.only(top: ScreenSize.absoluteHeight * 0.02),
-            decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: AppColors.text, width: 1)),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: AppColors.disabled, width: 1),
+            ),
             child: const Center(
               child: CircleAvatar(
-            
-                radius: 60,
+                radius: 55,
                 backgroundImage: AssetImage(AppAssets.avatar),
               ),
             ),
+          ),
+          Text(
+            AuthService.user!.name,
+            style: FontStyles.subtitle0(AppColors.text),
+          ),
+          Text(
+            AuthService.user!.email,
+            style: FontStyles.body0(AppColors.unfocused),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: ScreenSize.width * 0.03,
+              vertical: ScreenSize.absoluteHeight * 0.025,
+            ),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Card(
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                color: AppColors.appSecondary,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: ScreenSize.absoluteHeight * 0.022,
+                    horizontal: ScreenSize.width * 0.04,
+                  ),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.shopping_bag_sharp,
+                        color: AppColors.white,
+                        size: ScreenSize.absoluteHeight * 0.07,
+                      ),
+                      /*Text(
+                        '25',
+                        style: FontStyles.heading10(AppColors.white),
+                      ),*/
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Compras',
+                        style: FontStyles.bodyBold0(AppColors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: ScreenSize.width * 0.02,
+              ),
+              Card(
+                color: AppColors.appSecondary,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: ScreenSize.absoluteHeight * 0.022,
+                    horizontal: ScreenSize.width * 0.04,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        '25',
+                        style: FontStyles.heading10(AppColors.white),
+                      ),
+                      Text(
+                        'Compras',
+                        style: FontStyles.body1(AppColors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
           ),
         ],
       ),
